53017b22d65c2846734bb62ecc7da93e853e5b4b
bguina <benoit.guina@gmail.com>
2023-06-23 22:28:20 +0200

    :bricks: setup github project

---
+++ app/src/main/kotlin/com/bguina/eurosport/test/package-info.kt
@@ -0,0 +1,1 @@
+package com.bguina.eurosport.test
---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.46.1")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.0.2" apply false
+    id("com.android.library") version "8.0.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+}
+
+subprojects {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    val kotlinVersion by lazy {
+        plugins.withType<org.jetbrains.kotlin.gradle.plugin.KotlinBasePluginWrapper>()
+            .map { it.pluginVersion }.distinct().single()
+    }
+    configurations.forEach { config ->
+        config.resolutionStrategy.eachDependency {
+            if (requested.group == "org.jetbrains.kotlin" && requested.name.startsWith("kotlin-")) {
+                useVersion(kotlinVersion)
+            }
+        }
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions.jvmTarget = "17"
+    }
+}
