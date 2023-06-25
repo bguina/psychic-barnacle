638078a0bc69f17404fe778c4912ca8540a23571
bguina <benoit.guina@gmail.com>
2023-06-25 13:26:09 +0200

    :white_check_mark: ListSportArticlesUS sort&mix

--- app/src/main/kotlin/com/bguina/eurosport/test/data/ArticlesRepository.kt
+++ app/src/main/kotlin/com/bguina/eurosport/test/data/ArticlesRepository.kt
@@ -4,22 +4,20 @@ import com.bguina.eurosport.test.data.model.StoryEntity
 import com.bguina.eurosport.test.data.model.VideoEntity
 import com.bguina.eurosport.test.domain.IArticlesRepository
 import com.bguina.eurosport.test.domain.model.Article
-import java.lang.Integer.min
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ArticlesRepository @Inject constructor(
     private val articlesDataSource: IArticlesDataSource,
 ) : IArticlesRepository {
 
-    override suspend fun listArticles(): List<Article> {
-        val stories = articlesDataSource.listStories()
-        val videos = articlesDataSource.listVideos()
-        val maxArticles = min(stories.size, videos.size) * 2 + 1
-
-        return (0..maxArticles)
-            .map { listOf(stories.getOrNull(it)?.toDomain(), videos.getOrNull(it)?.toDomain()) }
-            .flatten()
-            .filterNotNull()
+    override suspend fun listArticlesByDateAsc(): List<Article> = withContext(Dispatchers.IO) {
+        val stories = async { articlesDataSource.listStories().map { it.toDomain() } }
+        val videos = async { articlesDataSource.listVideos().map { it.toDomain() } }
+        (stories.await() + videos.await())
+            .sortedWith(compareBy { it.date })
     }
 
     private fun StoryEntity.toDomain(): Article.Story = Article.Story(
--- app/src/main/kotlin/com/bguina/eurosport/test/domain/interactor/ListSportArticlesUseCase.kt
+++ app/src/main/kotlin/com/bguina/eurosport/test/domain/interactor/ListSportArticlesUseCase.kt
@@ -2,11 +2,28 @@ package com.bguina.eurosport.test.domain.interactor
 
 import com.bguina.eurosport.test.domain.IArticlesRepository
 import com.bguina.eurosport.test.domain.model.Article
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ListSportArticlesUseCase @Inject constructor(
     private val articlesRepository: IArticlesRepository,
 ) {
-    suspend operator fun invoke() : List<Article> = articlesRepository.listArticlesByDateAsc()
-        .sortedWith(compareBy { it.date })
+    suspend operator fun invoke(): List<Article> = withContext(Dispatchers.IO) {
+        articlesRepository.listArticlesByDateAsc()
+            .alternatingBetweenStoriesAndVideos()
+    }
+
+    private fun List<Article>.alternatingBetweenStoriesAndVideos(): List<Article> {
+        var awaitStory = true
+
+        return fold(emptyList()) { acc, article ->
+            if ((awaitStory && article is Article.Story) ||
+                (!awaitStory && article is Article.Video)
+            ) {
+                awaitStory = !awaitStory
+                acc + article
+            } else acc
+        }
+    }
 }
